215055315f661a210409cd231637210768d7a0ff
Wani <devwani93@gmail.com>
2023-04-19 16:08:59 +0900

    style: Logo 스타일 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <Link to="/" className="flex items-center text-lg cursor-pointer">
+    <Link to="/" className="flex items-center cursor-pointer">
       <BiShoppingBag className="mr-1" />
       <h1>React Shopping Challenge</h1>
     </Link>
