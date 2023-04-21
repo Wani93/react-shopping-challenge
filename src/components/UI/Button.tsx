7759878477086da5d0da991aca10f723c9fef40f
Wani <devwani93@gmail.com>
2023-04-21 16:13:24 +0900

    feat: UI 컴포넌트 Button 추가

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from 'react';
+
+interface Props {
+  text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({ text, onClick }: Props) => {
+  return (
+    <button className="px-2 rounded-lg bg-stone-300" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
